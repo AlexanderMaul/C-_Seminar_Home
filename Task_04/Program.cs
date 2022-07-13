95be1d507607a64c7aea9829e1ec46a4bdcd875d
Iskander <iskander-m.s@mail.ru>
2022-07-14 00:36:36 +0700

    Задача 4 решена

--- Task_04/Program.cs
+++ Task_04/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Программа поиска максимального числа из трёх ");
+Console.WriteLine("Введите первое целое число и нажмите Enter \nВведите второе целое число и нажмите Enter \nВведите третье целое число и нажмите Enter ");
+int num_A = Convert.ToInt32(Console.ReadLine());
+int num_B = Convert.ToInt32(Console.ReadLine());
+int num_C = Convert.ToInt32(Console.ReadLine());
+if(num_A > num_B && num_A > num_C) Console.WriteLine($"Число {num_A} является максимальным из трёх");
+else if(num_B > num_A && num_B > num_C) Console.WriteLine($"Число {num_B} является максимальным из трёх");
+else Console.WriteLine($"Число {num_C} является максимальным из трёх");
+
