2da0f51579b81334808949d80a3b77e2a4e9795d
Iskander <iskander-m.s@mail.ru>
2022-07-14 03:11:35 +0700

    Задача 7 решена

--- Task_07/Program.cs
+++ Task_07/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа
+
+Console.WriteLine("Программа нахождения еденичного разряда трёхзначного числа ");
+Console.WriteLine("Введите трёхзначное число число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999) 
+{
+    int erch = (num % 100) % 10;
+    Console.WriteLine($"Единичный разряд числа {num} равен {erch}.");
+}
+else if (num <= -100 && num >= -999) 
+{
+    int erch = (num % 100) % 10 *(-1);
+    Console.WriteLine($"Единичный разряд числа {num} равен {erch}.");
+}
+else Console.WriteLine($"Число {num} не является трёхзначным");
+
+
