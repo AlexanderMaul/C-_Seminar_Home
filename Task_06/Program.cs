ef30910f93b0bec4aad3e5262967bb9734b958c6
Iskander <iskander-m.s@mail.ru>
2022-07-14 02:12:04 +0700

    Задача 6 решена

--- Task_06/Program.cs
+++ Task_06/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 6. Написать программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+Console.WriteLine("Программа проверки чётности числа ");
+Console.WriteLine("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0) Console.WriteLine($"Число {num} является чётным");
+else Console.WriteLine($"Число {num} не является чётным");
+
