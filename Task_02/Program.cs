52b9dfb9b97e0461249b51a81391d8f65ef02be7
Iskander <iskander-m.s@mail.ru>
2022-07-13 23:40:12 +0700

    Задача 2 решена

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Программа сравненния двух чисел ");
+Console.WriteLine("Введите первое целое число и нажмите Enter \nВведите второе целое число и нажмите Enter ");
+int num_A = Convert.ToInt32(Console.ReadLine());
+int num_B = Convert.ToInt32(Console.ReadLine());
+if(num_A > num_B) Console.WriteLine($"Число {num_A} является большим, а число {num_B} является меньшим");
+else Console.WriteLine($"Число {num_B} является большим, а число {num_A} является меньшим");
+
