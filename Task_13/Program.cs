0e6d5f5680c4fb7fadc97cd0f9bd79953eb845e5
Iskander <iskander-m.s@mail.ru>
2022-07-28 05:43:22 +0700

    Задача 13 решена

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 13. Напишите программу, которая выводит третью цифру заданнаго числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Программа вывода третьей цифры заданного числа");
+Console.WriteLine("Введите число и нажмите Enter");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int third (int num)
+{
+    while (num >= 1000) num = num/10;
+    num = num % 10;
+    return num;
+}
+
+int Third = third(numA);
+string result = (numA < 100 && numA > -100) ? $"Третьей цифры нет" : Third.ToString();
+Console.WriteLine($"{numA} -> {result}");
+
