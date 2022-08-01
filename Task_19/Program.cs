fd751573586c8342565ccf4affb1f3f496dfd911
Iskander <iskander-m.s@mail.ru>
2022-08-02 01:54:06 +0700

    Задача 19 - некорректный вывод ответа

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно полиндромом
+
+Console.WriteLine("Программа проверки палиндромности пятизначного числа");
+Console.Write("Введите пятизначное число и нажмите Enter: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+string Plndr(int n)
+{
+    
+    if ((n < 10000 && n > 99999) || (n > -10000 && n < -99999)) return "Число не пятизначное";
+    if (n / 1000 == (n % 100 / 10) + (n % 10 * 10)) return $"{num} -> Да";
+    return $"{num} -> Нет";    
+}
+
+string result = Plndr(num);
+Console.WriteLine(result);
+
+
+
+
+
+
