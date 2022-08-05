4cf301b8e49cf578ff69eea317d188d63357ba3b
Iskander <iskander-m.s@mail.ru>
2022-08-05 11:45:43 +0700

    Задача 21 - ошибка исправлена

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -18,23 +18,27 @@ int y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Z: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 
-double Dist (int x1c, int y1c, int x2c, int y2c, int z1c, int z2c)
-{
-    int cat1 = 0;
-    int cat2 = 0;
-    int cat3 = 0;
-    double result = 0.0;
+// Вариант из семинара
+// double Dist (int x1c, int y1c, int x2c, int y2c, int z1c, int z2c)
+// {
+//     int cat1 = 0;
+//     int cat2 = 0;
+//     int cat3 = 0;
+//     double result = 0.0;
 
-    cat1 = Math.Abs(x1c) - Math.Abs(x2c);
-    cat2 = Math.Abs(y1c) - Math.Abs(y2c);
-    cat3 = Math.Abs(z1c) - Math.Abs(z2c);
-    result = Math.Sqrt(cat1 * cat1 + cat2 * cat2 + cat3 * cat3);
+//     cat1 = x1c - x2c;
+//     cat2 = y1c - y2c;
+//     cat3 = z1c - z2c;
+//     result = Math.Sqrt(cat1 * cat1 + cat2 * cat2 + cat3 * cat3);
+//     return Math.Round(result,2);
+// }
+
+// Упрощаем решение
+double Dist (int x1c, int y1c, int x2c, int y2c, int z1c, int z2c)
+{ 
+    double result = Math.Sqrt(Math.Pow(x1c - x2c, 2) + Math.Pow(y1c - y2c, 2) + Math.Pow(z1c - z2c, 2));
     return Math.Round(result,2);
 }
 
 double result = Dist(x1, y1, x2, y2, z1, z2);
 Console.WriteLine($"Растояние между точками A и B: {result}");
-
-// Для координат из условия A (3,6,8); B (2,1,-7), -> 15.84 ответ выдаёт 5,2,
-// а для коорлинат A (7,-5, 0); B (1,-1,9) -> 11.53 выдает 11,53.
-// Ошибка в примере?
